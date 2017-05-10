e6e6006e5046d68f9dba03b97dd7d9e4bf9de789
Anderson Madeira <andersonmadeiracs@gmail.com>
2017-05-09 23:59:40 -0300

    Tentativa de login melhoada

--- src/app/angular-firebase2/angular-firebase2.service.ts
+++ src/app/angular-firebase2/angular-firebase2.service.ts
@@ -13,6 +13,26 @@ export class AngularFirebase2Service {
     this.user = afAuth.authState;
   }
 
+  tryLoginOrRegister(email: string, password: string) {
+
+  }
+
+  registerWithEmailAndPassword(email: string, password: string) {
+    return this.afAuth.auth.createUserWithEmailAndPassword(email, password).
+      catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        if (errorCode == 'auth/weak-password') {
+          alert('The password is too weak.');
+        }
+        else if (errorCode == 'auth/email-already-in-use') {
+          alert('Email already in use!');
+        }
+        console.log(error);
+      });
+  }
+
   loginWithGoogle() {
     return this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
   }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -23,6 +23,36 @@ export class LoginComponent implements OnInit {
     });
   }
 
+  tryLoginOrRegister(email: string, password: string) {
+    this.afb.afAuth.auth.signInWithEmailAndPassword(email, password)
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      if (errorCode === 'auth/invalid-email') {
+        alert('O email informado é inválido!');
+      } else if (errorCode == 'auth/user-disabled') {
+        alert('Conta foi removida!');
+      } else if (errorCode == 'auth/wrong-password') {
+        alert('Senha inválida!');
+      } else if (errorCode == 'auth/user-not-found') {
+        this.afb.afAuth.auth.createUserWithEmailAndPassword(email, password).
+          catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            if (errorCode == 'auth/weak-password') {
+              alert('The password is too weak.');
+            }
+            console.log(error);
+          });
+        }
+      console.log(error);
+    });
+
+
+  }
+
   logout() {
     this.afb.logout();
   }
